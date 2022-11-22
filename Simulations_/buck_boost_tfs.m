c479599fddc9f88ad92f9fe3fbf0201c79a5cb04
Krz <jakubiakks@cardiff.ac.uk>
2022-11-22 10:35:25 +0000

    post meeting update 22/11/22, step function looks fine but PI control step function doesn't match

--- Simulations_/buck_boost_tfs.m
+++ Simulations_/buck_boost_tfs.m
@@ -13,11 +13,39 @@ delta_iL = 0.5;
 zeta = 1/sqrt(2);
 
 %% Inductor/Capacitor values
-Cb = Vo*T*D/2/Rb/delta_vo
-Lb = Vs*D*T/2/delta_iL
+Cb = Vo*T*D/2/Rb/delta_vo;
+Lb = Vs*D*T/2/delta_iL;
 % Results at D duty cyclce approximately confirm the ripple voltage. (constant D not PI)
 % Performance degrades above 0.5 duty cycle 
 
+%%
+s = tf('s');
+Gp = -Vs*(s*Lb*D/Rb/(1-D)^2-1)/(s^2*Lb*Cb+s*Lb/Rb+(1-D)^2)/Rb;
+kp = 0.0005;
+ki = 0.1;
+Gc = (s*kp+ki)/s;
+T = 1/(1+1/Gp/Gc);
+
+% num = Vs*(ki + kp*s)*(Rb + D^2*Rb - 2*D*Rb - D*Lb*s);
+% den = D^4*Rb^2*s - 4*D^3*Rb^2*s + Cb*Lb*D^2*Rb^2*s^3 + 6*D^2*Rb^2*s + Lb*D^2*Rb*s^2 + Vs*kp*D^2*Rb*s + Vs*ki*D^2*Rb - 2*Cb*Lb*D*Rb^2*s^3 - 4*D*Rb^2*s - 2*Lb*D*Rb*s^2 - 2*Vs*kp*D*Rb*s - 2*Vs*ki*D*Rb - Lb*Vs*kp*D*s^2 - Lb*Vs*ki*D*s + Cb*Lb*Rb^2*s^3 + Rb^2*s + Lb*Rb*s^2 + Vs*kp*Rb*s + Vs*ki*Rb;
+
+a = Lb*Cb*Rb*(1-D)^2;
+b = Lb*Rb*(1-D)^2-Lb*Vs*kp*D;
+c = Vs*kp*Rb*(1-D)^2*s + (1-D)^4*Rb^2*s - Lb*Vs*ki*D*s ;
+d = Vs*ki*Rb*(1-D)^2;
+
+T2 = 1/(a*s^3+b*s^2+c*s+d);
+
+
+% figure; pzmap(Gp);
+figure(1)
+pzmap(T);
+figure; step(T)
+xlim([0, 0.1])
+ylim([-0.2, 1.2])
+
+% hold on;
+% pzmap(T2);
 %% TF
 s = tf('s');
 Vo = Vs*D/(1-D);
@@ -40,7 +68,7 @@ xlim([0 0.02])
 % ylim([0 1.2])
 title('transfer function:step response')
 
-%% PI design
+%% PI design - analysis - incorrect
 syms vs s D Lb Rb Cb D_ kp ki Vs
 Gp = -Vs*(s*Lb*D/Rb/(1-D)^2-1)/(s^2*Lb*Cb+s*Lb/Rb+(1-D)^2)/Rb;
 Gc = (s*kp+ki)/s;
